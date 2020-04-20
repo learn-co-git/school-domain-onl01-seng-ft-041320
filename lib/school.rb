e7dc88d539cf6beb8543be9d06ec8a57f019246e
Neil Empey <argus.two.2@gmail.com>
2020-04-20 23:23:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+
+  attr_reader :roster, :name
+
+  def initialize(name)
+    @name = name
+    @roster = roster
+  end
+
+  def roster
+    @roster = {}
+  end
+
+
+  def add_student(student, grade)
+    
+  end
+
+
+end
+
+school = School.new("Washington")
+p school.name
+p school.roster
