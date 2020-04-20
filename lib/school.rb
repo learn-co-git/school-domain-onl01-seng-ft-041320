a39fbc0898ee35c2b96629874006fc96584a6c33
Neil Empey <argus.two.2@gmail.com>
2020-04-20 23:35:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,19 +1,21 @@
 class School
-
-  attr_reader :roster, :name
+  attr_accessor :roster
+  attr_reader :name
 
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-
   def add_student(student, grade)
-    
+      if @roster.has_key?(grade)
+        @roster[grade] << student
+      else
+        @roster[grade] = student.split(' ')
+      end
+    end
+
   end
-
-
-end
 
 school = School.new("Washington")
 p school.name
